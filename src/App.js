caa35ce89b7e8f14d94095a8fedfa7a1550f0b4a
yeonmm04 <yeonmm04@gmail.com>
2020-08-11 21:28:16 +0900

    비비

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import {Button, Progress} from 'semantic-ui-react'
 function App() {
   return (
     <div>
-    <p> 안녕하 세요 ! </p>
+    <p> 비비야 잘지내니 </p>
     <Button Primary> hi </Button>
     <Progress percent = {33} indicating/>
     </div>
