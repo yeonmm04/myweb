21d5aa5b6cfb95913e0cecd115063930e8a3ef3a
yeonmm04 <yeonmm04@gmail.com>
2020-08-13 19:42:52 +0900

    2

--- src/App.js
+++ src/App.js
@@ -1,15 +1,37 @@
 import React from 'react';
-import {Button} from 'semantic-ui-react'
+import {Button, Input} from 'semantic-ui-react'
 
 function Login(){
 
   return(
     <div>
-    <input/>
-    <button> 로그인 </button>
+    <Input/>
+    <Button onClick = { (e) => {
+      console.log(e)
+      alert(e.target.value)
+    } }> 로그인 </Button>
     </div>
 
   )
+}
+
+class Login extends React.Compnent{
+constructor(){
+  super()
+  this.state = (id : "없지렁")
+}
+  render(){
+    return(
+        <div>
+        <Input onchange = { (e) => this.setstate({id : e.target.value }) }/>
+        <Button value = {this.state.id} onClick = { (e) => {
+          console.log(e)
+          alert(e.target.value)
+        } }> Login </Button>
+        </div>
+)
+
+  }
 }
 
 function App() {
@@ -17,7 +39,7 @@ function App() {
     <div>
     <p> 비비야 잘지내니 </p>
     <button> 털 빗겨주기 </button>
-    <Button> 비비 밥주기 </Button>
+    <Button onClick = { () => alert("냠냠") }> 비비 밥주기 </Button>
     <Login />
     </div>
   );
