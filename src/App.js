88afc86969e4fc66254b6636b071e696836569e8
yeonmm04 <yeonmm04@gmail.com>
2020-08-13 19:15:58 +0900

    2

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import {Button, Progress} from 'semantic-ui-react'
+import {Button} from 'semantic-ui-react'
+
+function Login(){
+
+  return(
+    <div>
+    <input/>
+    <button> 로그인 </button>
+    </div>
+
+  )
+}
 
 function App() {
   return (
     <div>
     <p> 비비야 잘지내니 </p>
-    <Button Primary> 비비 밥주기 </Button>
-    <Progress percent = {33} indicating/>
+    <button> 털 빗겨주기 </button>
+    <Button> 비비 밥주기 </Button>
+    <Login />
     </div>
   );
 }
